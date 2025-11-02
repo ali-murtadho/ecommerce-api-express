aa1b6fa6f39c020d157e6123215cde67df6b9bde
Ali Murtadho <ali.murtadho@lawangsewu.com>
2025-11-02 14:43:01 +0700

    feat: middleware token dan file

---
+++ src/middlewares/upload.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'files')
+  },
+  filename: function (req, file, cb) {
+    const originalname = path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, originalname + '-' + file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage })
